1258f6f70feb6f1200f7088589aa662e3420bf98
jie <3228124082@qq.com>
2024-07-19 13:59:59 +0800

    上下文Context 将路由(router)独立出来，方便之后增强。 设计上下文(Context)，封装 Request 和 Response ，提供对 JSON、HTML 等返回类型的支持。

--- gee/gee.go
+++ gee/gee.go
@@ -4,33 +4,39 @@ import (
 	"net/http"
 )
 
-type handlerFunc func(http.ResponseWriter, *http.Request)
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+// Engine implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]handlerFunc
+	router *router
 }
 
+// New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: make(map[string]handlerFunc)}
+	return &Engine{router: newRouter()}
 }
-func (engine *Engine) addRoute(method string, patten string, handler handlerFunc) {
-	key := method + "-" + patten
-	engine.router[key] = handler
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
-func (engine *Engine) GET(patten string, handler handlerFunc) {
-	engine.addRoute("GET", patten, handler)
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
-func (engine *Engine) POST(patten string, handler handlerFunc) {
-	engine.addRoute("POST", patten, handler)
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
 }
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 not found"))
-	}
-}
-func (engine *Engine) Run(addr string) error {
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
